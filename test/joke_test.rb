b0ede3b86496770b3a0cefd24d25ebd8a31f5b5d
smunozjr1986 <sergiomunozjr07@gmail.com>
2017-12-13 10:58:48 -0700

    project

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -5,4 +5,21 @@ require './lib/joke'
 
 class JokeTest < Minitest::Test
 
+  def test_it_exist
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_instance_of Joke, joke
+  end
+
+def test_joke_id
+  joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+  assert_equal 1, joke.id
+
+end
+
+
+
+
+
 end
